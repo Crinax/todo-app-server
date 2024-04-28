d766453746a94195c69a5556623b9bf52eef88c2
Crinax <agreedeth@gmail.com>
2024-04-28 03:02:29 +0300

    feat(core/services/test): add test for task creation

---
+++ src/core/services/task/__tests__/create_task.rs
@@ -0,0 +1,43 @@
+use crate::core::{
+    entities::{
+        rules::{min_len_string::MinLenString, string_based_id::StringBasedId, BuisnessRule},
+        task::Task,
+    },
+    ports::secondary::task::CreateTaskPort,
+};
+
+struct CreateTaskAdapter {}
+
+impl CreateTaskPort for CreateTaskAdapter {
+    type CreateTaskPortError = ();
+
+    async fn create_task(
+        &self,
+        name_base: &MinLenString<1>,
+        description_base: &MinLenString<0>,
+    ) -> Result<Task, Self::CreateTaskPortError> {
+        let id = StringBasedId::parse("1".to_owned()).unwrap();
+        let name = name_base.clone();
+        let desctription = description_base.clone();
+
+        // some async logic here :D
+
+        Ok(Task::new(id, name, desctription))
+    }
+}
+
+#[tokio::test]
+async fn create_task_success_case() {
+    let adapter = CreateTaskAdapter {};
+    let result = adapter
+        .create_task(
+            &MinLenString::parse("1234".to_owned()).unwrap(),
+            &MinLenString::parse("1234".to_owned()).unwrap(),
+        )
+        .await
+        .unwrap();
+
+    assert_eq!(result.id(), "1");
+    assert_eq!(result.name(), "1234");
+    assert_eq!(result.description(), "1234");
+}
--- src/core/services/task/__tests__/mod.rs
+++ src/core/services/task/__tests__/mod.rs
@@ -1,3 +1,4 @@
+mod create_task;
 mod get_all_tasks;
 mod get_task;
 mod search_tasks;
