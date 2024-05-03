038caf5da0a87aed366d2e0b3e51ebbee11c8df2
Crinax <agreedeth@gmail.com>
2024-05-03 17:45:45 +0300

    feat(entities/tests): add removing tests for task_window

--- src/core/entities/__tests__/task_window.rs
+++ src/core/entities/__tests__/task_window.rs
@@ -39,3 +39,40 @@ fn task_window_cannot_add_task_with_same_id() {
 
     assert_eq!(task_window.tasks(), &HashSet::from([task_1]));
 }
+
+#[test]
+fn task_window_can_remove_task() {
+    let task_1 = Task::new(
+        "1".parse().unwrap(),
+        "task 1".parse().unwrap(),
+        "simple task".parse().unwrap(),
+    );
+
+    let task_2 = Task::new(
+        "2".parse().unwrap(),
+        "task 2".parse().unwrap(),
+        "simple task".parse().unwrap(),
+    );
+
+    let mut task_window = TaskWindow::new();
+
+    task_window.add(task_1.clone());
+    task_window.add(task_2.clone());
+
+    assert_eq!(task_window.remove(task_2.id()), Some(task_2));
+    assert_eq!(task_window.tasks(), &HashSet::from([task_1]));
+}
+
+#[test]
+fn task_window_cannot_remove_non_existing_task() {
+    let task_1 = Task::new(
+        "1".parse().unwrap(),
+        "task 1".parse().unwrap(),
+        "simple task".parse().unwrap(),
+    );
+
+    let mut task_window = TaskWindow::new();
+
+    assert_eq!(task_window.remove(task_1.id()), None);
+    assert_eq!(task_window.tasks(), &HashSet::from([]));
+}
