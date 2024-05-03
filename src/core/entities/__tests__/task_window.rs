d5db0793dced6278b9e913b580fc81b47b9b4f02
Crinax <agreedeth@gmail.com>
2024-05-03 17:36:44 +0300

    feat(entities/tests): add insertion test with same id for task_window

--- src/core/entities/__tests__/task_window.rs
+++ src/core/entities/__tests__/task_window.rs
@@ -23,3 +23,19 @@ fn task_window_can_add_task_with_different_id() {
 
     assert_eq!(task_window.tasks(), &HashSet::from([task_1, task_2]));
 }
+
+#[test]
+fn task_window_cannot_add_task_with_same_id() {
+    let task_1 = Task::new(
+        "1".parse().unwrap(),
+        "task 1".parse().unwrap(),
+        "simple task".parse().unwrap(),
+    );
+
+    let mut task_window = TaskWindow::new();
+
+    task_window.add(task_1.clone());
+    task_window.add(task_1.clone());
+
+    assert_eq!(task_window.tasks(), &HashSet::from([task_1]));
+}
