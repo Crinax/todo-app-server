7621b50034542df2e44ed6bb895ebbbdf484d054
Crinax <agreedeth@gmail.com>
2024-05-05 02:21:31 +0300

    feat(entities): add new entity window

--- src/core/entities/mod.rs
+++ src/core/entities/mod.rs
@@ -3,6 +3,7 @@ pub mod stage;
 pub mod stage_window;
 pub mod task;
 pub mod task_window;
+pub mod window;
 
 #[cfg(test)]
 mod __tests__;
---
+++ src/core/entities/window.rs
@@ -0,0 +1,79 @@
+use std::cmp::Ordering;
+
+use crate::core::entities::rules::string_based_id::StringBasedId;
+
+pub trait Id {
+    fn id(&self) -> &StringBasedId;
+}
+
+pub trait Order<T: Eq + PartialEq + PartialOrd + Ord> {
+    fn order(&self) -> T;
+}
+
+pub trait SortableWindow<T: Id + Order<O>, O: Eq + PartialEq + PartialOrd + Ord> {
+    fn sort_by_key<F: Fn(&T) -> O>(&mut self, f: F);
+    fn sort<F: FnMut(&T, &T) -> Ordering>(&mut self, f: F);
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct Window<T: Id> {
+    collection: Vec<T>,
+}
+
+impl<T: Id> From<Vec<T>> for Window<T> {
+    fn from(collection: Vec<T>) -> Self {
+        Self { collection }
+    }
+}
+
+impl<T: Id + Clone, const N: usize> From<[T; N]> for Window<T> {
+    fn from(collection: [T; N]) -> Self {
+        Self {
+            collection: collection.to_vec(),
+        }
+    }
+}
+
+impl<T: Id> Window<T> {
+    pub fn new() -> Self {
+        Self { collection: vec![] }
+    }
+
+    pub fn has(&self, id: &StringBasedId) -> bool {
+        self.collection.iter().any(|item| item.id() == id)
+    }
+
+    pub fn collection(&self) -> &[T] {
+        &self.collection
+    }
+
+    pub fn add(&mut self, item: T) {
+        if !self.has(item.id()) {
+            self.collection.push(item);
+        }
+    }
+
+    pub fn remove(&mut self, id: &StringBasedId) -> Option<T> {
+        let index = self.collection.iter().position(|item| item.id() == id);
+
+        index.map(|i| self.collection.remove(i))
+    }
+
+    pub fn get(&self, id: &StringBasedId) -> Option<&T> {
+        self.collection.iter().find(|item| item.id() == id)
+    }
+
+    pub fn get_mut(&mut self, id: &StringBasedId) -> Option<&mut T> {
+        self.collection.iter_mut().find(|item| item.id() == id)
+    }
+}
+
+impl<T: Id + Order<O>, O: Eq + PartialEq + PartialOrd + Ord> SortableWindow<T, O> for Window<T> {
+    fn sort_by_key<F: Fn(&T) -> O>(&mut self, f: F) {
+        self.collection.sort_by_key(f);
+    }
+
+    fn sort<F: FnMut(&T, &T) -> Ordering>(&mut self, f: F) {
+        self.collection.sort_by(f);
+    }
+}
