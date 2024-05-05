9a13cd43e967b345770431108a4a4f3783c0814e
Crinax <agreedeth@gmail.com>
2024-05-05 03:55:00 +0300

    fix(queries/stage): make all trait functions are async

--- src/core/ports/primary/queries/stage/mod.rs
+++ src/core/ports/primary/queries/stage/mod.rs
@@ -3,11 +3,11 @@ use crate::core::entities::{rules::string_based_id::StringBasedId, stage_window:
 pub trait GetAllStagesQuery {
     type GetAllStagesQueryError;
 
-    fn get_all_stages(&self) -> Result<StageWindow, Self::GetAllStagesQueryError>;
+    async fn get_all_stages(&self) -> Result<StageWindow, Self::GetAllStagesQueryError>;
 }
 
 pub trait GetStageQuery {
     type GetStageQueryError;
 
-    fn get_stage(&self, id: &StringBasedId) -> Result<StageWindow, Self::GetStageQueryError>;
+    async fn get_stage(&self, id: &StringBasedId) -> Result<StageWindow, Self::GetStageQueryError>;
 }
