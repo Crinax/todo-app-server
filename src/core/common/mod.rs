191fceb7e8a7021a54e0331ee49a5843b64ee7ba
Crinax <agreedeth@gmail.com>
2024-09-24 11:42:51 +0300

    chore(commit): another verification test

--- src/core/common/mod.rs
+++ src/core/common/mod.rs
@@ -11,9 +11,8 @@ pub trait AsShared {
     fn as_atomic_shared(self) -> Arc<Mutex<Self>>;
 }
 
-// for test commit
 // TODO: Сначала переделать на воркспейсы
 // #[proc_macro_derive(AsShared)]
 // pub fn derive_as_shared(input: TokenStreamсф) -> TokenStream {
-    
+
 // }
